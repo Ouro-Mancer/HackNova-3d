0af1bc7071f3008f7f37c075578dcd8c619af27b
Neha Sharma <139962072+SharmaNehaJava@users.noreply.github.com>
2025-05-02 20:57:00 +0530

    added footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Hero from './sections/Hero'
 import NavBar from './sections/Navbar'
+import Footer from './sections/Footer'
+
 
 const App = () => {
   return (
     <div className='text-center bg-[#131225] min-h-screen text-white text-4xl'>
       <NavBar/>
       <Hero />
+      <Footer/>
     </div>
   )
 }
